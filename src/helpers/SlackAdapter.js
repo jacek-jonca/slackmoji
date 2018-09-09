46aed2d106798a86398490629d9e1e7ff61ea5cf
nikymorg <nicole.morgan@gmail.com>
2018-09-09 16:23:21 -0400

    change search positioning

--- src/helpers/SlackAdapter.js
+++ src/helpers/SlackAdapter.js
@@ -3,7 +3,7 @@ export default class SlackAdapter {
   static getSlackmoji() {
     const url = process.env.REACT_APP_API_URL
     return fetch(url)
-      .then(r=> r.json())
+      .then(r => r.json())
       .then(r => r.body)
   }
 
