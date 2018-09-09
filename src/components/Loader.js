fc425a8f0d60531a914ae9b716b7f0717645d73e
nikymorg <nicole.morgan@gmail.com>
2018-09-09 17:54:01 -0400

    typing load

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,28 +1,36 @@
 import React, {Component} from 'react'
-import randomImage from '../helpers/randomImage'
 
 export default class Loader extends Component {
-  state = {src: randomImage()}
+  state = {text: ''}
 
   componentDidMount() {
-    this.interval = setInterval(() => {
-      const src = randomImage()
-      this.setState({src})
-    }, 1000)
+    this.i = 0
+    this.text = "Checking on those 'mojis..."
+    this.showText()
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
+  showText = () => {
+    const {i, text} = this
+    if (i < text.length) {
+      setTimeout(this.showText, 60)
+      this.setState((prevState) => ({text: prevState.text + text.charAt(i)}),
+      () => this.i = i+1
+    )}
   }
 
   render() {
     return (
-      <div className='loader flex center'>
+      <div className='loader flex column center-cross'>
+      <div>
         <img
-          src={this.state.src}
-          alt='me'
-          className='load-image'
+        src='faces/heart-ring.png'
+        alt='me'
+        className='load-image'
         />
+      </div>
+        <h2 className='loading-text center-text'>
+          {this.state.text}
+        </h2>
       </div>
     )
   }
