0acbd66832971804a4384decc2a0a1cf91e0c998
nikymorg <nicole.morgan@gmail.com>
2018-10-14 09:11:14 -0400

    sort by tag length

--- src/components/BitmojiCard.js
+++ src/components/BitmojiCard.js
@@ -2,27 +2,28 @@ import React, {Component} from 'react'
 import {filterTags, imageSrc} from '../helpers/bitmoji'
 
 export default class BitmojiCard extends Component {
-  state = {count: 3}
+  state = {count: 2}
 
   toggleTags = () => {
     this.setState(prevState => {
       const {count} = prevState
       const {bitmoji: {tags}} = this.props
-      const newCount = (count === 3) ? tags.length : 3
+      const newCount = (count === 2) ? tags.length : 2
       return {count: newCount}
     })
   }
 
   render() {
     const {bitmoji: {src, tags}} = this.props
-    const displayTags = filterTags(tags).slice(0, this.state.count)
+    const filteredTags = filterTags(tags)
+    const displayTags = filteredTags.slice(0, this.state.count)
 
     return (
       <li className='bitmoji-card flex-container column' onClick={this.toggleTags}>
         <img src={imageSrc(src)} alt={`bitmoji ${tags[0]}`} />
         <ul className='flex column margin-m'>
-          {displayTags.map(tag => <li key={tag}>'{tag}'</li>)}
-          {this.state.count === 3 &&
+          {displayTags.map(tag => <li key={tag}>"{tag}"</li>)}
+          {displayTags.length < filteredTags.length &&
             <li className='center-self-cross font-bold'>. . .</li>
           }
         </ul>
--- src/helpers/bitmoji.js
+++ src/helpers/bitmoji.js
@@ -1,11 +1,18 @@
 const filterTags = (tags) => {
   const filteredTags = tags.map(tag => tag.replace(/\*/, ''))
+  sortByLength(filteredTags)
   return [...new Set(filteredTags)]
+}
+
+const sortByLength = (tags) => {
+  tags.sort()
+  return tags.reverse()
 }
 
 const imageSrc = (src) => {
   const url = src.replace(/%s/g, process.env.REACT_APP_BITMOJI_ID)
   return url + '&width=200'
 }
+
 
 export {filterTags, imageSrc}
