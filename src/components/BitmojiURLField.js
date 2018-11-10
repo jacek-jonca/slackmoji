74072e3ebdb2db623b92d40bcd2a737b45c23a76
nikymorg <nicole.morgan@gmail.com>
2018-11-10 10:20:58 -0500

    bitmoji url tooltip

--- src/components/BitmojiURLField.js
+++ src/components/BitmojiURLField.js
@@ -30,13 +30,16 @@ export default class BitmojiURLField extends Component {
           className='url-form flex space-between'
         >
           <input
-            className='margin-m grow-1'
+            className='margin-m grow-1 tooltip'
             type='text'
             value={this.state.url}
             onChange={this.handleChange}
             placeholder='Enter URL of any image starring your Bitmoji'
           />
           <input className='btn center-self-cross' type='submit' value={btnText}/>
+          <span className='tooltip-text'>
+            <img src='./statue.gif' alt='tim-statue'/>
+          </span>
         </form>
       </Fragment>
     )
