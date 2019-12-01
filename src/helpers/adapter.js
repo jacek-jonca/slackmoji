58518237f02f63506c23f20303c516e56419ebdd
Niky Morgan <niky@nikym.org>
2019-12-01 11:15:48 -0500

    remove axios

--- src/helpers/adapter.js
+++ src/helpers/adapter.js
@@ -1,9 +1,8 @@
-import axios from 'axios'
-
 const getSlackmoji = () => {
   const url = process.env.REACT_APP_API_URL
-  return axios.get(url)
-    .then(r => r.data.body)
+  return fetch(url)
+    .then(r => r.json())
+    .then(r=> r.body)
 }
 
 export { getSlackmoji }
