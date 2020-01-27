60fcf08dfde85a221128954d8cbc02a5a3b40f1b
Niky Morgan <nikycode@gmail.com>
2020-01-26 19:27:58 -0500

    notice of move

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {copyToClipboard} from '../helpers/url'
 
+const Notice = () => (
+  <div className='flex column grow-1 notice-margin'>
+    <div>Deprecation Notice: this website has been relocated to <a href='https://slackmoji.netlify.com/'>https://slackmoji.netlify.com/</a>.</div>
+    <div>Please adjust your bookmarks as this url will be removed as of June 1, 2020.</div>
+  </div>
+)
+
 const Header = () => {
   const handleClick = () => copyToClipboard(window.location)
-
+  const showNotice = !!window.location.host.match('firebaseapp.com')
   return (
     <div className='header center-text'>
       <div className='flex margin-ta'>
         <button className='btn btn-sm' onClick={handleClick}>
           Copy URL
         </button>
+        { showNotice &&
+          <Notice /> }
       </div>
       <h1>
         SlackMoji
