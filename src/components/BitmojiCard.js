03efe04ffa7d7d26abc725ddad1c53833906fbd0
Niky M <20468684+nikymorg@users.noreply.github.com>
2020-03-14 20:15:44 -0400

    justify arrow tag bottom (#63)

--- src/components/BitmojiCard.js
+++ src/components/BitmojiCard.js
@@ -50,15 +50,15 @@ const BitmojiCard = ({
             </span>
           </li>)
         )}
-        { moreTags
-        ? <li className='tag-arrow' onClick={toggleTags}>
-          &#x25BC;
-        </li>
-        : <li className='tag-arrow' onClick={toggleTags}>
-          &#x25B2;
-        </li>
-        }
       </ul>
+      { moreTags
+      ? <div className='tag-arrow' onClick={toggleTags}>
+        &#x25BC;
+      </div>
+      : <div className='tag-arrow' onClick={toggleTags}>
+        &#x25B2;
+      </div>
+      }
     </li>
   )
 }
